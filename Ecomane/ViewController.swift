7f176fa9c1c945d9e667f45c3b573bdb945861b4
SHiN201007 <shin1007m@gmail.com>
2019-11-10 23:04:39 +0900

    first push

---
+++ Ecomane/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ecomane
+//
+//  Created by 松丸真 on 2019/11/10.
+//  Copyright © 2019 松丸真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
