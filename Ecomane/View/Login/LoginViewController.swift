3b685ad8b23176a4d7e273ccfe05d2f0905a1bad
SHiN201007 <shin1007m@gmail.com>
2019-11-12 12:17:36 +0900

    Login/Register UI 作成

--- Ecomane/View/Login/LoginViewController.swift
+++ Ecomane/View/Login/LoginViewController.swift
@@ -14,15 +14,50 @@ class LoginViewController: UIViewController {
   @IBOutlet weak var emailField: UITextField!
   @IBOutlet weak var passwordField: UITextField!
   @IBOutlet weak var loginButton: UIButton!
+  @IBOutlet weak var registerButton: UIButton!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-
-
+    
+    assignbackground()
+    customCSS()
   }
 
   @IBAction func loginButton(_ sender: Any) {
     login()
+  }
+  
+  func assignbackground(){
+      let background = UIImage(named: "harinezumi")
+
+      var imageView : UIImageView!
+      imageView = UIImageView(frame: view.bounds)
+      imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+      imageView.clipsToBounds = true
+      imageView.image = background
+      imageView.center = view.center
+      view.addSubview(imageView)
+      self.view.sendSubviewToBack(imageView)
+  }
+  
+  func customCSS() {
+    registerButton.backgroundColor = .white
+    registerButton.layer.cornerRadius = 13.0
+    registerButton.setTitleColor(.black, for: .normal)
+    registerButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+    
+    loginButton.backgroundColor = .systemPink
+    loginButton.layer.cornerRadius = 13.0
+    loginButton.setTitleColor(.white, for: .normal)
+    loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+  }
+  
+  @IBAction func registerButton(_ sender: Any) {
+    if let vc = UIStoryboard(name: "Register", bundle: nil).instantiateViewController(
+      withIdentifier: "Register") as? RegisterViewController {
+      vc.title = "新規会員登録"
+      self.navigationController?.pushViewController(vc, animated: true)
+    }
   }
   
   func login() {
--- Ecomane/View/Register/RegisterViewController.swift
+++ Ecomane/View/Register/RegisterViewController.swift
@@ -14,16 +14,52 @@ class RegisterViewController: UIViewController {
   @IBOutlet weak var emailField: UITextField!
   @IBOutlet weak var passwordField: UITextField!
   @IBOutlet weak var registerButton: UIButton!
+  @IBOutlet weak var loginButton: UIButton!
   
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    assignbackground()
+    customCSS()
+  }
+  
+  func assignbackground(){
+      let background = UIImage(named: "harinezumi")
+
+      var imageView : UIImageView!
+      imageView = UIImageView(frame: view.bounds)
+      imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+      imageView.clipsToBounds = true
+      imageView.image = background
+      imageView.center = view.center
+      view.addSubview(imageView)
+      self.view.sendSubviewToBack(imageView)
+  }
+  
+  func customCSS() {
+    loginButton.backgroundColor = .white
+    loginButton.layer.cornerRadius = 13.0
+    loginButton.setTitleColor(.black, for: .normal)
+    loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
     
+    registerButton.backgroundColor = .systemPink
+    registerButton.layer.cornerRadius = 13.0
+    registerButton.setTitleColor(.white, for: .normal)
+    registerButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
   }
 
   @IBAction func registerButton(_ sender: Any) {
     register()
   }
+  
+  @IBAction func loginButton(_ sender: Any) {
+    if let vc = UIStoryboard(name: "Login", bundle: nil).instantiateViewController(
+      withIdentifier: "Login") as? LoginViewController {
+      vc.title = "ログイン"
+      self.navigationController?.pushViewController(vc, animated: true)
+    }
+  }
+  
   
   func register() {
    Auth.auth().createUser(withEmail: emailField.text ?? "", password: passwordField.text ?? "") { (user, error) in
